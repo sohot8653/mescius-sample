425aa77f109d908e7bf87106bf5e9993c60cd4eb
sohot8653 <kmbin92@gmail.com>
2024-01-30 23:49:58 +0900

    [엑셀 활용 페이지 추가 개발]

--- app.js
+++ app.js
@@ -7,10 +7,23 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views/ejs'));
 app.use(express.static(path.join(__dirname, 'static')));
 app.use(expressLayouts);
+
+app.get("/sample15", function(req, res) {
+    let {depth1, depth2, sampleId, sample23Index} = req.query;
+    res.render("sample"+sampleId+".ejs", {depth1, depth2, sample23Index});
+});
+
 app.get("/sample", function(req, res) {
     let {depth1, depth2, sampleId} = req.query;
-    res.render("sample"+sampleId+".ejs", {depth1, depth2});
+    let param = {depth1, depth2};
+    
+    if(sampleId == 15) {
+        param.sample23Index = req.query.sample23Index ?? -1;
+    }
+    
+    res.render("sample"+sampleId+".ejs", param);
 });
+
 app.get("/", function(req, res) {
     res.redirect('/sample?depth1=BMS&depth2=표준원가&sampleId=1');
 });
